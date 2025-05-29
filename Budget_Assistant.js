8f4b688851359933575f571f16d6ff5f8e43cbd8
Ramiro Arellano <ramiroarellano@Ramiros-MacBook-Air.local>
2025-05-29 14:31:42 -0400

    Update Budget_Assistant.js

--- Budget_Assistant.js
+++ Budget_Assistant.js
@@ -8,5 +8,8 @@ let NetCashFlow= monthlyrevenue - monthlyexpenses;
 let CurrentBalance= initialcapital + NetCashFlow;
 let profitability=NetCashFlow > 0;
 
-console.log('Startup Name;Net Cash Flow;Current Balance;Profitability');
-console.log(`${startupName};${NetCashFlow};${CurrentBalance};${profitability}`);
+console.log('Startup Name;;', startupName);
+console.log('Net monthly cash flow;;', NetCashFlow);
+console.log('Current Balance;;', CurrentBalance);
+console.log('Is the startup profitable?;;', profitability ? 'TRUE' : 'FLASE');
+
