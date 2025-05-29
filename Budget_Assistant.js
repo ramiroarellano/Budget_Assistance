c2d6bfe1873bea45da61202fc9857af2b5083a73
Ramiro Arellano <ramiroarellano@Ramiros-MacBook-Air.local>
2025-05-29 14:59:19 -0400

    Update Budget_Assistant.js

--- Budget_Assistant.js
+++ Budget_Assistant.js
@@ -1,15 +1,14 @@
 console.log("Hello, My Friends!");
-let startupName = "Nummo Foundation"; 
-let initialcapital= 10000;
-let monthlyrevenue= 4500;
-let monthlyexpenses= 3000;
+let startUpName = "Nummo Foundation"; 
+let initialCapital= 10000;
+let monthlyRevenue= 4500;
+let monthlyExpenses= 3000;
 
-let NetCashFlow= monthlyrevenue - monthlyexpenses;
-let CurrentBalance= initialcapital + NetCashFlow;
-let profitability=NetCashFlow > 0;
+let netCashFlow= monthlyRevenue - monthlyExpenses;
+let currentBalance= initialCapital + netCashFlow;
+let profitability=netCashFlow > 0;
 
-console.log('Startup Name;;', startupName);
-console.log('Net monthly cash flow;;', NetCashFlow);
-console.log('Current Balance;;', CurrentBalance);
-console.log('Is the startup profitable?;;', profitability ? 'TRUE' : 'FLASE');
-
+console.log('Startup Name:', startUpName);
+console.log('Net monthly cash flow:', netCashFlow);
+console.log('Current Balance:', currentBalance);
+console.log('Is the startup profitable?:', profitability ? 'TRUE' : 'FLASE');
